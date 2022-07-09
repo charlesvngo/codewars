6a1664c5ca75d791c761d67d3a73c3bc46e87f08
Charles Ngo <charlesngo37@gmail.com>
2022-07-08 21:23:08 -0700

    solve for numbers 0 to 999

--- parseInt.js
+++ parseInt.js
@@ -1,5 +1,7 @@
 const parseInt = (string) => {
+  // list of strings to numbers from 0 - 99. These will be combined to make any number
   const digits = {
+    zero: 0,
     one: 1,
     two: 2,
     three: 3,
@@ -21,11 +23,38 @@ const parseInt = (string) => {
     nineteen: 19,
     twenty: 20,
     thirty: 30,
-    fourty: 40,
+    forty: 40,
     fifty: 50,
     sixty: 60,
     seventy: 70,
     eighty: 80,
     ninety: 90,
   };
+
+  if (string.includes("hundred")) {
+    const numArray = string
+      .split("hundred")
+      .map((word) => word.replace("and ", "").trim())
+      .map((word, index) =>
+        index === 0 ? parseInt(word) * 100 : parseInt(word)
+      );
+
+    if (numArray[1] === undefined) {
+      return numArray[0];
+    } else {
+      return numArray.reduce((prev, curr) => prev + curr);
+    }
+  }
+
+  if (string.includes("-")) {
+    const numArray = string
+      .split("-")
+      .map((word) => parseInt(word))
+      .reduce((previousValue, currentValue) => previousValue + currentValue);
+    return numArray;
+  }
+
+  return digits[string];
 };
+
+console.log(parseInt("one hundred and one"));
