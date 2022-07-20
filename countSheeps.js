cbad5ba18000ea235948c9a8c0759f756b1b12fe
Charles Ngo <charlesngo37@gmail.com>
2022-07-19 23:53:16 -0700

    solve countsheep

---
+++ countSheeps.js
@@ -0,0 +1,32 @@
+const countSheeps = (arrayOfSheeps) => {
+  return arrayOfSheeps.filter((x) => x).length;
+};
+
+console.log(
+  countSheeps([
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ])
+);
