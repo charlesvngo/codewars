5446f649e93ccdb7410e51b67fca7957d2b680bc
Charles Ngo <charlesngo37@gmail.com>
2022-07-22 14:29:44 -0700

    solve same.js with a better method

--- same.js
+++ same.js
@@ -1,15 +1,16 @@
 const comp = (array1, array2) => {
-  let output = true;
-  const array3 = array1.map((e) => array2.filter((x) => x === e * e));
-  for (const item of array3) {
-    if (!item.length) {
-      output = false;
-    }
+  if (!array1 || !array2 || array1.length !== array2.length) return false;
+  if (array1.length === 0 && array2.length === 0) return true;
+
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] * array1[i] !== array2[i]) return false;
   }
-  return output;
+  return true;
 };
 
-const a1 = [11, 19, 19, 19, 121, 144, 144, 161, 161];
+const a1 = [11, 19, 19, 19, 121, 144, 144, 161];
 const a2 = [
   11 * 11,
   19 * 19,
