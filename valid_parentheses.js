371eb8d01adb395eb076c398eca52077fb790dec
Charles Ngo <charlesngo37@gmail.com>
2022-07-25 21:01:06 -0700

    first attempt, incorrect logic present

--- valid_parentheses.js
+++ valid_parentheses.js
@@ -4,5 +4,37 @@
  */
 
 const isValid = (s) => {
-  splitString = s.split("");
+  // separate the string into elements of an array
+  const splitString = s.split("");
+
+  //
+  for (let i = splitString.length - 1; i >= 0; i--) {
+    if (
+      splitString[i] === ")" &&
+      splitString.indexOf("(") !== -1 &&
+      splitString.indexOf("(") < i
+    ) {
+      splitString.splice(i, 1);
+      splitString.splice(splitString.indexOf("("), 1);
+    }
+    if (
+      splitString[i] === "]" &&
+      splitString.indexOf("[") !== -1 &&
+      splitString.indexOf("[") < i
+    ) {
+      splitString.splice(i, 1);
+      splitString.splice(splitString.indexOf("["), 1);
+    }
+    if (
+      splitString[i] === "}" &&
+      splitString.indexOf("{") !== -1 &&
+      splitString.indexOf("{") < i
+    ) {
+      splitString.splice(i, 1);
+      splitString.splice(splitString.indexOf("{"), 1);
+    }
+  }
+  return splitString.length === 0;
 };
+
+console.log(isValid("([)]"));
