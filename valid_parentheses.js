02d99219b9f3d56fba5ae0a6927044e241fff1be
Charles Ngo <charlesngo37@gmail.com>
2022-07-26 17:01:18 -0700

    solve is valid parenthesis

--- valid_parentheses.js
+++ valid_parentheses.js
@@ -4,37 +4,27 @@
  */
 
 const isValid = (s) => {
-  // separate the string into elements of an array
-  const splitString = s.split("");
+  // Create a map to identify the other complimentary bracket
+  const brackets = {
+    "}": "{",
+    "]": "[",
+    ")": "(",
+  };
 
-  //
-  for (let i = splitString.length - 1; i >= 0; i--) {
-    if (
-      splitString[i] === ")" &&
-      splitString.indexOf("(") !== -1 &&
-      splitString.indexOf("(") < i
-    ) {
-      splitString.splice(i, 1);
-      splitString.splice(splitString.indexOf("("), 1);
-    }
-    if (
-      splitString[i] === "]" &&
-      splitString.indexOf("[") !== -1 &&
-      splitString.indexOf("[") < i
-    ) {
-      splitString.splice(i, 1);
-      splitString.splice(splitString.indexOf("["), 1);
-    }
-    if (
-      splitString[i] === "}" &&
-      splitString.indexOf("{") !== -1 &&
-      splitString.indexOf("{") < i
-    ) {
-      splitString.splice(i, 1);
-      splitString.splice(splitString.indexOf("{"), 1);
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+      stack.push(s[i]);
+    } else {
+      if (brackets[s[i]] === stack[stack.length - 1]) {
+        stack.pop();
+      } else return false;
     }
   }
-  return splitString.length === 0;
+
+  if (stack.length === 0) return true;
+  else return false;
 };
 
-console.log(isValid("([)]"));
+console.log(isValid("()"));
