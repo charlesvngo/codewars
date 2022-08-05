a75ff6d81b807a02d3fb80773aeae4fe20c44d30
Charles Ngo <charlesngo37@gmail.com>
2022-08-05 15:10:19 -0700

    created the array

---
+++ sum_of_odd.js
@@ -0,0 +1,32 @@
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+const rowSumOddNumbers = (n) => {
+  // create an array of all the required odd numbers.
+  let array = [];
+  let arrayLength = 0;
+  let output = 0;
+
+  for (let i = 0; i <= n; i++) {
+    arrayLength += i;
+  }
+  for (let i = 0; i < arrayLength; i++) {
+    array.push(1 + i * 2);
+  }
+
+  return output;
+  // sum the last n numbers
+};
+
+console.log(rowSumOddNumbers(3));
