5facb9a1fbfd954646198f6bb764bdbaea979907
Charles Ngo <charlesngo37@gmail.com>
2022-08-16 23:43:25 -0700

    solved count stairs

--- climbingStairs.js
+++ climbingStairs.js
@@ -33,23 +33,37 @@
 // Example 4:
 
 // Input: n = 5
-// Output: 3
+// Output: 8
 // Explanation: There are three ways to climb to the top.
 // 1. 1 step + 1 step + 1 step + 1 step + 1 step
-// 1. 1 step + 1 step + 1 step + 2 steps
-// 1. 1 step + 1 step + 2 steps + 1 step
-// 1. 1 step + 2 steps + 1 step + 1 step
-// 1. 2 steps + 1 step + 1 step + 1 step
-// 1. 1 step + 2 steps + 2 steps
-// 1. 2 steps + 1 step + 2 steps
-// 1. 2 steps + 2 steps + 1 step
+// 2. 1 step + 1 step + 1 step + 2 steps
+// 3. 1 step + 1 step + 2 steps + 1 step
+// 4. 1 step + 2 steps + 1 step + 1 step
+// 5. 2 steps + 1 step + 1 step + 1 step
+// 6. 1 step + 2 steps + 2 steps
+// 7. 2 steps + 1 step + 2 steps
+// 8. 2 steps + 2 steps + 1 step
 
 // Constraints:
 
 //     1 <= n <= 45
 
+// pattern looks like the fibonacci sequence
+
 /**
  * @param {number} n
  * @return {number}
  */
-const climbStairs = (n) => {};
+const climbStairs = (n) => {
+  const fibonacci = [0, 1];
+
+  for (let i = 1; i <= n; i++) {
+    const prev = fibonacci[i - 1];
+    const curr = fibonacci[i];
+    fibonacci.push(prev + curr);
+  }
+
+  return fibonacci.pop();
+};
+
+console.log(climbStairs(5));
