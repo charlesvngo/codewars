54fe880e38ce9f53dd88b610c66da7d818e44e66
Charles Ngo <charlesngo37@gmail.com>
2022-08-22 21:01:52 -0700

    initial commit for sameTree.js

--- inorderTraversal.js
+++ inorderTraversal.js
@@ -37,11 +37,22 @@
 
 const inorderTraversal = (root) => {
   const output = [];
+
   if (root === null) {
     return output;
   }
 
-  // Stack to travel down the tree. Will move left first then when it reaches the bottom, traverse up then right.
+  /**
+   * The goal is to maintain a stack of nodes to visit as we traverse
+   * down the tree. As we traverse down, We go left and push all the
+   * left nodes first in the stack. Once we reach to the bottom, we
+   * store the node value and traverse right.
+   *           1
+   *         /   \
+   *        2     3    preorder traversal: 4 -> 2 -> 5 -> 1 -> 6 -> 3
+   *       / \   /     (left -> root -> right)
+   *      4   5 6
+   */
   const stack = [];
   let curr = root;
 
