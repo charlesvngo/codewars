d67d1645c2ff36d58528f139cd7ff1803da4b7ac
Charles Ngo <charlesngo37@gmail.com>
2022-08-26 23:03:18 -0700

    added recommended solution

--- endsWith.js
+++ endsWith.js
@@ -1,4 +1,4 @@
-// Complete the solution so that it returns true if the first argument(string) 
+// Complete the solution so that it returns true if the first argument(string)
 // passed in ends with the 2nd argument (also a string).
 
 // Examples:
@@ -6,10 +6,20 @@
 // solution('abc', 'bc') // returns true
 // solution('abc', 'd') // returns false
 
+const solution = (str, ending) => {
+  const string = str.split("").reverse();
+  const end = ending.split("").reverse();
 
-const solution(str, ending){
-  const string = str.split("").reverse()
-  const end = ending.split("").reverse()
+  for (let i = 0; i < end.length; i++) {
+    if (end[i] !== string[i]) {
+      return false;
+    }
+  }
 
+  return true;
+};
 
-}
+// Clever and recommended solution uses javascript built in method endsWith()
+// const solution = (str, ending){
+//   return str.endsWith(ending);
+// }
